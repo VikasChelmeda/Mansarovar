f0c4dec893f174e12ffcb7a9a8d6550d06669e6e
vikasADT <vikas@annamrajus.com>
2024-03-27 11:53:44 +0530

    mansarovar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter,Routes,Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/categories" element={<Navbar />} />
     </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logoImage from "../Images/Manasarovar.png";
+import profileimages from "../Images/943e7e0891056899a8f9d2b286be6250.png";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-container">
                 <div className="logo">
+                    <Link to="/">
+                        <img src={logoImage} alt="" />
+                    </Link>
+                </div>
+                <div className="nav-links">
                     <ul>
-
+                        <li><Link to="/categories">Categories |</Link></li>
+                        <li><Link to="/resorts">Resorts |</Link></li>
+                        <li><Link to="/gurus">Gurus |</Link></li>
+                        <li><Link to="/groups">Groups |</Link></li>
                     </ul>
                 </div>
             </div>
+            <div className="profile">
+                    <img src={profileimages} alt="" />
+                </div>
         </nav>
     );
 }
 
+export default Navbar;
